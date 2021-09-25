3a10dc9991e5e621562977698bca97d05868ea6e
Yeasir Ahamed <yahamed007@gmail.com>
2021-09-25 16:51:16 +0600

    use error handler

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
+import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import morgan from 'morgan';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoute.js';
@@ -24,6 +26,13 @@ app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
 app.use('/api/upload', uploadROutes);
+
+const __dirname = path.resolve();
+
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 8000;
 
